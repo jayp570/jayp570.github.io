81ae2a3c1673e7bea4709ac3a2ff9da36fa525f3
jayp-png <60831681+jayp-png@users.noreply.github.com>
2023-01-19 21:19:05 -0800

    add redactle clone

---
+++ miscellaneous/censorle/articles.js
@@ -0,0 +1,1140 @@
+let vitalArticlesRaw = `Current total (997 articles)
+People (112 articles)
+Leaders and politicians (26 articles)
+ Hammurabi
+  Hatshepsut
+  Ramesses II
+  Cyrus the Great
+ Alexander the Great
+ Ashoka
+ Qin Shi Huang
+  Julius Caesar
+ Augustus
+  Charlemagne
+ Genghis Khan
+ Mansa Musa
+ Joan of Arc
+  Suleiman the Magnificent
+  Akbar
+ Elizabeth I
+ Catherine the Great
+ George Washington
+   Napoleon
+ Simón Bolívar
+  Abraham Lincoln
+  Mahatma Gandhi
+ Joseph Stalin
+ Adolf Hitler
+ Mao Zedong
+ Nelson Mandela
+Religious figures (8 articles)
+ Abraham
+  Moses
+ Gautama Buddha
+ Jesus
+  Paul the Apostle
+ Muhammad
+  Adi Shankara
+   Martin Luther
+Explorers (7 articles)
+ Marco Polo
+ Zheng He
+  Christopher Columbus
+ Vasco da Gama
+  Ferdinand Magellan
+ James Cook
+ Roald Amundsen
+
+Philosophers and social scientists (18 articles)
+  Confucius
+  Laozi
+ Herodotus
+ Socrates
+  Plato
+ Aristotle
+ Cicero
+  Thomas Aquinas
+ Ibn Khaldun
+ Niccolò Machiavelli
+  René Descartes
+  John Locke
+  Adam Smith
+ Immanuel Kant
+ Mary Wollstonecraft
+ Karl Marx
+ Friedrich Nietzsche
+  Sigmund Freud
+Writers (11 articles)
+ Homer
+ Virgil
+ Li Bai
+ Murasaki Shikibu
+  Rumi
+ Dante Alighieri
+ Miguel de Cervantes
+ William Shakespeare
+ Voltaire
+ Leo Tolstoy
+  Rabindranath Tagore
+Artists (6 articles)
+  Leonardo da Vinci
+  Michelangelo
+  Rembrandt
+ Hokusai
+ Pablo Picasso
+ Frida Kahlo
+Musicians and composers (6 articles)
+   Johann Sebastian Bach
+  Wolfgang Amadeus Mozart
+ Ludwig van Beethoven
+  Louis Armstrong
+ The Beatles
+ Michael Jackson
+
+Scientists and inventors (19 articles)
+  Hippocrates
+ Avicenna
+ Shen Kuo
+ Johannes Gutenberg
+  Nicolaus Copernicus
+  Galileo Galilei
+  Isaac Newton
+ Carl Linnaeus
+  Antoine Lavoisier
+ Michael Faraday
+ Charles Darwin
+ Florence Nightingale
+ Louis Pasteur
+ James Clerk Maxwell
+ Dmitri Mendeleev
+  Thomas Edison
+ Nikola Tesla
+ Marie Curie
+  Albert Einstein
+Mathematicians (8 articles)
+ Archimedes
+ Euclid
+ Muhammad ibn Musa al-Khwarizmi
+ Leonhard Euler
+  Carl Friedrich Gauss
+ Emmy Noether
+ Kurt Gödel
+ Alan Turing
+Filmmakers (2 articles)
+ Charlie Chaplin
+ Walt Disney
+Businesspeople (1 article)
+ Henry Ford
+
+History (83 articles)
+General (4 articles)
+ History (Level 2)
+   Human history (Level 1)
+ Civilization (Level 2)
+  Archaeology
+History by region (9 articles)
+ History of Africa
+ History of Asia
+ History of East Asia
+ History of India
+ History of the Middle East
+ History of Europe
+ History of North America
+ History of Oceania
+ History of South America
+History by subject (11 articles)
+  History of science (Level 2)
+ History of art
+ History of agriculture
+ History of architecture
+ History of film
+ History of literature
+ History of mathematics
+ History of medicine
+ History of music
+ History of technology
+ Military history
+Prehistory (4 articles)
+ Prehistory (Level 2)
+  Stone Age
+ Early human migrations
+ Neolithic Revolution
+Ancient history (18 articles)
+ Ancient history (Level 2)
+ Bronze Age
+ Ancient Egypt
+ Indus Valley civilisation
+ Mesopotamia
+ Sumer
+ Phoenicia
+ Iron Age
+ Ancient Greece
+  Ancient Rome
+  Achaemenid Empire
+ Gupta Empire
+ Han dynasty
+ Silk Road
+ Pre-Columbian era
+ Andean civilizations
+ Mesoamerica
+ Maya civilization
+Post-classical history (13 articles)
+ Post-classical history (Level 2)
+ Aztecs
+  Inca Empire
+ Islamic Golden Age
+ Middle Ages
+  Black Death
+ Byzantine Empire
+  Crusades
+ Holy Roman Empire
+ Viking Age
+ Mongol Empire
+  Ottoman Empire
+ Tang dynasty
+Modern history (24 articles)
+ Early modern period (Level 2)
+ Renaissance
+  Age of Discovery
+ European colonization of the Americas
+ Western imperialism in Asia
+ Spanish Empire
+ Reformation
+ Mughal Empire
+ Scientific Revolution
+ Age of Enlightenment
+ Late modern period (Level 2)
+  British Empire
+   American Revolution
+  French Revolution
+  Industrial Revolution
+ Scramble for Africa
+  World War I
+  Soviet Union
+  Great Depression
+ World War II
+ Decolonization
+  Cold War
+ Green Revolution
+ Information Age
+Geography (106 articles)
+General (1 article)
+ Geography (Level 2)
+Continents and regions (11 articles)
+ Continent
+ Africa (Level 2)
+ Antarctica
+ Asia (Level 2)
+  Europe (Level 2)
+ North America (Level 2)
+  South America (Level 2)
+Geographical regions
+ Arctic
+ Caribbean
+ Middle East
+ Oceania (Level 2)
+Terrestrial features (12 articles)
+ Land (Level 2)
+ Desert
+  Sahara
+ Forest
+ Amazon rainforest
+ Grassland
+ Island
+ Mountain
+ Alps
+ Andes
+ Himalayas
+ Rocky Mountains
+Hydrological features (19 articles)
+ Sea (Level 2)
+ Arctic Ocean
+  Atlantic Ocean
+ Mediterranean Sea
+ Indian Ocean
+ Pacific Ocean
+  Great Barrier Reef
+ Southern Ocean
+ Lake
+ Caspian Sea
+  Great Lakes
+ Lake Victoria
+ River
+ Amazon River
+  Ganges
+  Mississippi River
+ Nile
+ Yangtze
+  Glacier
+Countries (43 articles)
+ Country (Level 2)
+Africa
+   Algeria
+ Democratic Republic of the Congo
+   Egypt
+ Ethiopia
+ Kenya
+  Nigeria
+  South Africa
+ Tanzania
+Asia
+  Bangladesh
+  China
+ India
+  Indonesia
+  Iran
+  Israel
+ Japan
+ Malaysia
+ Myanmar
+  Pakistan
+ Philippines
+ Saudi Arabia
+  South Korea
+   Taiwan
+ Thailand
+  United Arab Emirates
+  Vietnam
+Eurasia
+ Russia
+  Turkey
+Europe
+  France
+ Germany
+ Italy
+ Netherlands
+ Poland
+ Spain
+   Ukraine
+   United Kingdom
+North America
+ Canada
+  Mexico
+   United States
+Oceania
+ Australia
+South America
+  Argentina
+   Brazil
+  Colombia
+Cities (20 articles)
+  City (Level 2)
+Africa
+ Cairo
+  Lagos
+Asia
+East Asia
+ Beijing
+  Hong Kong
+  Tokyo
+Southeast Asia
+ Jakarta
+ Singapore
+India
+  Delhi
+  Mumbai
+Western Asia
+  Istanbul
+  Jerusalem
+  Mecca
+Europe
+ London
+  Moscow
+  Paris
+ Rome
+North America
+ Mexico City
+  New York City
+South America
+ São Paulo
+Arts (45 articles)
+General (5 articles)
+ The arts (Level 1)
+  Art
+ Prehistoric art
+ Fashion
+ Museum
+Artistic movements (4 articles)
+ Abstract art
+ Modernism
+ Realism
+  Romanticism
+Architecture (3 articles)
+ Architecture (Level 2)
+  Great Pyramid of Giza
+ Great Wall of China
+Literature (8 articles)
+ Literature (Level 2)
+ English literature
+ Fiction
+ Novel
+ Short story
+  Fairy tale
+  Poetry
+ Epic poetry
+Music (9 articles)
+ Music (Level 2)
+ Musical instrument
+  Piano
+ Singing
+Musical genres
+ Classical music
+  Folk music
+  Jazz
+ Pop music
+  Rock music
+Performing arts (5 articles)
+ Performing arts (Level 2)
+ Dance
+ Opera
+ Orchestra
+ Theatre
+Visual arts (11 articles)
+ Visual arts (Level 2)
+  Film (Level 2)
+ Animation
+ Calligraphy
+ Comics
+ Design
+ Drawing
+ Painting
+ Photography
+ Pottery
+  Sculpture
+Philosophy and religion (55 articles)
+Philosophy (17 articles)
+General (2 articles)
+ Philosophy (Level 1)
+ Philosophy of science
+Philosophical branches and concepts (12 articles)
+ Aesthetics
+  Epistemology
+ Knowledge (Level 2)
+ Belief
+ Reason
+ Truth
+ Ethics (Level 2)
+ Good and evil
+ Logic (Level 2)
+ Metaphysics
+  Free will
+ Ontology
+Philosophy by region and period (3 articles)
+ Eastern philosophy
+ Confucianism
+ Western philosophy
+Mythology (2 articles)
+ Myth
+  Greek mythology
+Religion (13 articles)
+ Religion (Level 2)
+ Afterlife
+ Deity (Level 2)
+   God
+  Meditation
+ New religious movement
+  Prayer
+ Ritual
+ Shamanism
+ Soul
+ Spirituality
+ Secularism
+ Atheism
+Specific religions (23 articles)
+Abrahamic religions (11 articles)
+  Christianity
+  Bible
+ Catholic Church
+ Eastern Orthodox Church
+ Protestantism
+  Islam
+ Shia Islam
+ Sunni Islam
+ Quran
+  Judaism
+  Talmud
+East Asian religions (3 articles)
+ Chinese folk religion
+ Shinto
+ Taoism
+Indian religions (8 articles)
+  Buddhism
+ Mahayana
+ Theravada
+  Hinduism
+  Vedas
+   Bhagavad Gita
+ Jainism
+  Sikhism
+Other religions (1 article)
+ Traditional African religions
+Everyday life (57 articles)
+General (4 articles)
+ Clothing (Level 2)
+ Home (Level 2)
+ Furniture
+ Jewellery
+Family and kinship (10 articles)
+ Ethnic group (Level 2)
+ Family (Level 2)
+ Adult
+ Adolescence
+ Child
+ Infant
+ Marriage
+ Old age
+ Parenting
+ Friendship
+Sexuality and gender (6 articles)
+ Human sexuality (Level 2)
+ Sexual intercourse
+ Sexual orientation
+  Gender
+ Man
+ Woman
+Food and drink (22 articles)
+ Cooking
+  Food (Level 2)
+ Bread
+ Cereal
+  Wheat
+ Maize
+  Rice
+  Cheese
+  Fruit
+ Meat
+ Salt
+ Spice
+ Sugar
+ Vegetable
+ Potato
+ Soybean
+ Drink
+  Alcoholic drink
+   Coffee
+ Drinking water
+  Milk
+  Tea
+Recreation and entertainment (15 articles)
+ Entertainment (Level 2)
+ Play (activity)
+ Game
+ Board game
+ Card game
+ Gambling
+   Video game
+ Sport (Level 2)
+  Association football
+ Sport of athletics
+ Olympic Games
+  Toy
+ Martial arts
+ Swimming
+ Tourism
+Society and social sciences (146 articles)
+General (13 articles)
+  Culture (Level 2)
+ Folklore (Level 2)
+ Festival
+ Oral tradition
+ Popular culture
+ Society (Level 1)
+  Community
+ Power (social and political)
+ Social class
+ Communication (Level 2)
+ Social science
+  Anthropology
+ Sociology
+Politics and government (25 articles)
+   Politics (Level 2)
+ Political party
+ Political science
+ Colonialism
+  Imperialism
+ Government (Level 2)
+ Democracy
+ Dictatorship
+ Monarchy
+ Ideology
+ Anarchism
+   Capitalism
+ Communism
+ Conservatism
+ Fascism
+  Liberalism
+ Nationalism
+ Socialism
+ State
+  Diplomacy
+ Military
+International organizations
+  European Union
+  International Red Cross and Red Crescent Movement
+ NATO
+ United Nations
+Conflict (4 articles)
+ War (Level 2)
+ Genocide
+ Peace
+ Terrorism
+Education (4 articles)
+ Education (Level 2)
+ School
+ Library
+ University
+Business and economics (22 articles)
+ Business (Level 2)
+ Corporation
+ Management
+ Marketing
+ Retail
+ Trade union
+  Economics (Level 2)
+ Trade (Level 2)
+  Supply and demand
+ Employment
+ Finance
+ Bank
+  Money
+ Insurance
+  Tax
+ Economy
+ Agriculture (Level 2)
+ Manufacturing (Level 2)
+ Construction
+ Fishing
+ Hunting
+ Mining
+Social issues (21 articles)
+   Abortion
+ Disability
+ Discrimination
+  Racism
+ Sexism
+ Environmentalism
+ Pollution
+ Famine
+ Feminism
+ Women's suffrage
+  Globalization
+ Human migration
+ Human rights
+ Liberty
+ Privacy
+ Slavery
+ Social equality
+ Indigenous peoples
+ Poverty
+ Violence
+ Welfare
+Law (6 articles)
+  Law (Level 2)
+  Crime
+ Constitution
+ Justice
+  Police
+ Property
+Psychology (16 articles)
+ Psychology (Level 2)
+ Emotion (Level 2)
+  Anger
+ Fear
+  Happiness
+ Humour
+ Love
+ Mind (Level 2)
+ Consciousness
+ Dream
+ Memory
+ Thought
+ Human behavior
+   Intelligence
+ Learning
+ Personality
+Language (31 articles)
+ Language (Level 2)
+  Indo-European languages
+  Bengali
+ English
+ French
+  German
+ Greek
+ Hindustani
+   Latin
+  Portuguese
+  Russian
+ Spanish
+Other languages
+ Arabic
+ Chinese
+ Japanese
+ Malay
+ Swahili
+ Linguistics
+ Grammar
+ Word
+ Personal name
+ Speech
+ Writing (Level 2)
+   Alphabet
+ Arabic script
+ Brahmic scripts
+ Cyrillic script
+   Greek alphabet
+ Latin script
+ Arabic numerals
+ Chinese characters
+Media (4 articles)
+ Mass media
+ Broadcasting
+ News
+  Publishing
+Health, medicine and disease (41 articles)
+Illness and injury (19 articles)
+ Disease (Level 2)
+ Allergy
+  Asthma
+  Cancer
+ Cardiovascular disease
+ Stroke
+ Diabetes
+ Gastroenteritis
+ Infection
+ Common cold
+ Influenza
+ Malaria
+  Pneumonia
+ Sexually transmitted infection
+  HIV/AIDS
+ Smallpox
+  Tuberculosis
+ Mental disorder
+  Injury
+Health, fitness, and medicine (13 articles)
+ Medicine (Level 2)
+ Dentistry
+ Hospital
+  Nursing
+ Surgery
+ Ageing
+ Exercise
+ Health
+ Mental health
+ Hygiene
+ Sanitation
+  Nutrition
+ Obesity
+Drugs and medication (9 articles)
+ Drug
+ Medication
+ Anesthesia
+ Antibiotic
+ Birth control
+ Vaccine
+ Addiction
+ Alcoholism
+  Smoking
+Science (209 articles)
+General (5 articles)
+ Science (Level 1)
+ Scientific method
+ Measurement
+ International System of Units
+ Nature
+Astronomy (26 articles)
+ Astronomy (Level 2)
+ Universe (Level 2)
+ Solar System (Level 2)
+ Sun (Level 2)
+ Mercury
+ Venus
+ Earth (Level 1)
+ Moon (Level 2)
+ Mars
+  Jupiter
+ Saturn
+ Uranus
+ Neptune
+   Asteroid
+  Big Bang
+  Black hole
+  Comet
+ Galaxy
+ Milky Way
+ Natural satellite
+ Orbit
+ Outer space
+ Physical cosmology
+ Planet
+ Star
+ Supernova
+Physics (41 articles)
+  Physics (Level 2)
+ Energy (Level 2)
+  Time (Level 2)
+ Day
+ Year
+ Classical mechanics
+ Mass
+ Momentum
+ Motion
+ Newton's laws of motion
+ Force
+ Electromagnetism
+  Gravity
+ Strong interaction
+ Weak interaction
+ Magnetism
+ Matter
+ State of matter
+  Atom (Level 2)
+ Particle physics
+  Standard Model
+ Subatomic particle
+ Electron
+  Neutron
+  Photon
+ Proton
+  Quantum mechanics
+ Radioactive decay
+ Space
+ Vacuum
+  Thermodynamics
+ Heat
+ Temperature
+ Theory of relativity
+ Speed of light
+ Wave
+ Electromagnetic radiation
+  Light
+ Color
+ Optics
+  Sound
+Biology (82 articles)
+ Biology (Level 2)
+  Life (Level 1)
+  Cell (Level 2)
+  Death (Level 2)
+ Suicide
+ Abiogenesis
+ Evolution (Level 2)
+ Human evolution
+  Natural selection
+ Organism
+ Archaea
+ Bacteria
+ Eukaryote
+ Animal (Level 2)
+ Zoology
+ Amphibian
+ Arthropod
+ Crustacean
+   Insect
+ Bird
+ Chicken
+ Fish
+ Mammal
+  Cat
+ Cattle
+  Dog
+ Horse
+ Pig
+ Primate
+  Human (Level 1)
+ Rodent
+ Sheep
+ Mollusca
+ Reptile
+ Dinosaur
+ Plant (Level 2)
+ Algae
+ Botany
+ Flower
+ Seed
+ Tree
+ Fungus
+ Virus
+ Anatomy (Level 2)
+ Human body
+ Circulatory system
+ Blood
+ Heart
+ Lung
+ Digestion
+ Liver
+ Immune system
+ Skeletal muscle
+ Nervous system
+ Brain
+ Ear
+ Eye
+ Sense
+ Skeleton
+ Bone
+ Skin
+ Ecology (Level 2)
+ Biodiversity
+  Ecosystem
+  Extinction
+ Genetics
+ DNA
+  Gene
+ Heredity
+ RNA
+ Metabolism
+ Molecular biology
+ Hormone
+ Protein
+ Paleontology
+ Photosynthesis
+ Reproduction
+ Sex
+ Pregnancy
+ Sleep
+ Taxonomy
+ Species
+Chemistry (32 articles)
+  Chemistry (Level 2)
+ Biochemistry
+ Inorganic chemistry
+ Organic chemistry
+ Physical chemistry
+ Chemical element (Level 2)
+ Periodic table
+ Aluminium
+ Carbon
+ Copper
+  Gold
+ Hydrogen
+ Iron
+ Nitrogen
+ Oxygen
+ Phosphorus
+ Silicon
+  Silver
+ Sulfur
+ Chemical compound
+  Water (Level 2)
+  Carbon dioxide
+ Chemical bond
+ Molecule
+ Chemical reaction
+ Acid–base reaction
+ Catalysis
+ Redox
+ Metal
+ Alloy
+ Bronze
+ Steel
+Earth science (23 articles)
+ Earth science
+  History of Earth
+ Atmosphere of Earth
+  Structure of Earth
+ Season
+ Flood
+ Climate (Level 2)
+ Climate change
+ Weather
+ Cloud
+ Rain
+ Snow
+  Tornado
+  Tropical cyclone
+ Wind
+ Geology (Level 2)
+  Earthquake
+ Erosion
+ Mineral
+  Plate tectonics
+ Rock
+  Soil
+  Volcano
+Technology (98 articles)
+General (4 articles)
+  Technology (Level 1)
+ Engineering (Level 2)
+ Civil engineering
+  Mechanical engineering
+Energy (14 articles)
+ Electricity (Level 2)
+  Electric battery
+ Fire (Level 2)
+ Explosive
+ Gunpowder
+ Fossil fuel
+ Coal
+ Natural gas
+ Petroleum
+ Nuclear power
+  Renewable energy
+ Hydropower
+   Solar energy
+  Wind power
+Food and health (10 articles)
+ Animal husbandry
+ Domestication
+ Biotechnology
+ Genetic engineering
+ Fertilizer
+ Food preservation
+ Garden
+ Medical imaging
+ Refrigeration
+ Stove
+Weapons (6 articles)
+ Weapon
+ Armour
+ Bow and arrow
+ Firearm
+ Knife
+  Nuclear weapon
+Tools and machinery (9 articles)
+ Tool (Level 2)
+ Simple machine
+ Wheel
+ Engine
+ Electric motor
+ Internal combustion engine
+  Jet engine
+ Steam engine
+ Robotics
+Media and communication (10 articles)
+ Printing
+ Book (Level 2)
+  Mail
+ Telecommunications
+ Internet
+ Radio
+ Telephone
+ Mobile phone
+ Video
+  Television
+Computing and information technology (4 articles)
+ Computer science
+ Computer (Level 2)
+ Artificial intelligence
+  Cryptography
+Electronics (4 articles)
+ Electronics
+ Electric light
+ Integrated circuit
+ Semiconductor device
+Space (5 articles)
+  Rocket
+ Satellite
+ Space exploration
+ Spaceflight
+ Space station
+Transportation (6 articles)
+ Transport (Level 2)
+ Aircraft
+  Bicycle
+ Car
+ Rail transport
+ Ship
+Navigation and timekeeping (6 articles)
+ Navigation
+ Compass
+ Map
+  Radar
+ Calendar
+ Clock
+Structures (6 articles)
+ Fortification
+ Infrastructure
+ Bridge
+ Canal
+ Dam
+ Road
+Materials (9 articles)
+ Concrete
+  Glass
+ Masonry
+ Metallurgy
+ Natural rubber
+ Paper
+ Plastic
+ Textile
+  Wood
+Optical (5 articles)
+ Camera
+ Laser
+ Lens
+ Microscope
+ Telescope
+Mathematics (45 articles)
+General (5 articles)
+  Mathematics (Level 1)
+  Algorithm
+ Mathematical proof
+ Set
+ Function
+Counting and numbers (12 articles)
+ Combinatorics
+  Number (Level 2)
+ Real number
+ E_(mathematical_constant)
+ Pi
+ Fraction
+ Integer
+ 0
+ Natural number
+ Prime number
+ Complex number
+  Number theory
+Algebra (5 articles)
+  Algebra
+ Equation
+ Variable
+  Linear algebra
+ Abstract algebra
+Analysis (5 articles)
+  Mathematical analysis
+  Calculus
+ Infinity
+ Limit
+ Series
+Arithmetic (4 articles)
+ Arithmetic (Level 2)
+ Exponentiation
+ Logarithm
+ nth root
+Geometry and topology (12 articles)
+ Geometry (Level 2)
+ Angle
+  Trigonometry
+  Area
+ Conic section
+ Circle
+ Line
+ Polygon
+  Triangle
+ Three-dimensional space
+ Volume
+  Topology
+Probability and statistics (2 articles)
+ Probability
+  Statistics (Level 2)`
+
+let vitalArticlesRawList = vitalArticlesRaw.split("\n")
+//console.log(vitalArticlesRawList)
+let vitalArticles = []
+for(let item of vitalArticlesRawList) {
+    if(item != "") {
+        if(!item.includes("article)") && !item.includes("articles)")) {
+            vitalArticles.push(item)
+        }
+    }
+}
+for(let i = 0; i < vitalArticles.length; i++) {
+    vitalArticles[i] = vitalArticles[i].trim()
+    if(vitalArticles[i].indexOf("(Level") != -1) {
+        vitalArticles[i] = vitalArticles[i].substring(0, vitalArticles[i].indexOf("(Level")-1)
+    }
+    vitalArticles[i] = vitalArticles[i].replaceAll(" ", "_")
+}
+//console.log(vitalArticles)
+
+document.getElementById("stuff").innerHTML = vitalArticles.join("<br>")
---
+++ miscellaneous/censorle/index.js
@@ -0,0 +1,225 @@
+let displayText = ""
+let articleText = ""
+let data
+let articleName = vitalArticles[Math.floor(Math.random()*vitalArticles.length)]
+//articleName = "pet door"
+const commonWords = ["a","aboard","about","above","across","after","against","along","amid","among","an","and","around","as","at","because","before","behind","below","beneath","beside","between","beyond","but","by","concerning","considering","despite","down","during","except","following","for","from","if","in","inside","into","is","it","like","minus","near","next","of","off","on","onto","opposite","or","out","outside","over","past","per","plus","regarding","round","save","since","than","the","through","till","to","toward","under","underneath","unlike","until","up","upon","versus","via","was","with","within","without"]
+let guesses = []
+let guessCounts = {}
+let articleWords
+
+settingsPopup.style.display = "none"
+victoryPopup.style.display = "none"
+popupBackground.style.display = "none"
+
+let won = false
+
+let notCensored = [" ", ",", ".", "-", "(", ")", ";", ":", "'", "[", "]", "/"]
+
+const http = new XMLHttpRequest()
+
+http.open("GET", `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=${articleName}&redirects=true&origin=*`)
+http.send()
+
+http.onload = () => {
+    data = http.responseText
+    data = JSON.parse(data)
+    data = data.query.pages[Object.keys(data.query.pages)[0]]
+    load()
+}
+
+function load() {
+    console.log(data)
+    console.log(data.extract)
+    document.getElementById("article").innerHTML = `<h1>${data.title}</h1>`+data.extract
+    articleText = document.getElementById("article").innerHTML
+
+    document.getElementById("guesses").innerHTML = ""
+
+    let inTag = false
+    for(let i = 0; i < articleText.length; i++) {
+        let char = articleText[i]
+        if(char == "<") {
+            inTag = true
+        }
+
+        if(!inTag && !notCensored.includes(char)) {
+            displayText += "█"
+        } else {
+            displayText += char
+        }
+
+        if(char == ">") {
+            inTag = false
+        }
+    }
+
+    console.log(displayText)
+
+    document.getElementById("article").innerHTML = displayText
+
+    for(let commonWord of commonWords) {
+        reveal(commonWord)
+    }
+
+    //reveal("2")
+    //reveal("zone")
+    //reveal("tourism")
+
+}
+
+window.addEventListener('keydown', keyDownHandler, false);
+function keyDownHandler(e) {
+    let code = e.keyCode;
+    guessInput = document.getElementById("guessInput")
+    if(code == 13 && guessInput == document.activeElement) {
+        guessWord = guessInput.value
+        guessCount = reveal(guessWord.trim())
+        guessInput.value = ""
+
+        if(guessCount != -1) {
+            //add to guess list html
+            let num = guesses.length-commonWords.length
+            document.getElementById("guesses").innerHTML = `
+            <div id="guess">
+                <span id="guessWord">${num}. ${guessWord}</span>
+                <span id="guessCount">${guessCount}</span>
+            </div>
+            ` + document.getElementById("guesses").innerHTML
+        }
+    }
+}
+
+
+function reveal(guess) {
+
+    if(!guesses.includes(guess) && guess != "" && articleText != displayText) {
+        console.log("did guess")
+        guesses.push(guess)
+
+        let count = 0
+
+        let allRevealed = false
+        let lastIndex = 0
+        while(!allRevealed) {
+            let newDisplayText = ""
+            let index = articleText.toLowerCase().indexOf(guess.toLowerCase(), lastIndex)
+            //console.log(index)
+            if(displayText[index] == "█" && displayText[index-1] != "█" && displayText[index+guess.length] != "█") {
+                // for(let i = 0; i < articleText.length; i++) {
+                //     if(i >= index && i < index+guess.length) {
+                //         newDisplayText += articleText[i]
+                //     } else {
+                //         newDisplayText += displayText[i]
+                //     }
+                // }
+                newDisplayText = displayText.substring(0, index-1) + articleText.substring(index-1, index+guess.length) + displayText.substring(index+guess.length)
+                //console.log(articleText.substring(index-5, index+guess.length+10))
+                displayText = newDisplayText
+                count++
+
+            }
+            //console.log(newDisplayText)
+            document.getElementById("article").innerHTML = displayText
+
+            lastIndex = index+1
+            if(articleText.toLowerCase().indexOf(guess.toLowerCase(), lastIndex) == -1) {
+                allRevealed = true
+            }
+
+        }
+
+        let h1 = displayText.substring(0, displayText.indexOf("</h1>"))
+        if(!h1.includes("█")) {
+            won = true
+            toggleVictoryPopup()
+            displayText = articleText
+            document.getElementById("article").innerHTML = displayText
+        }
+        guessCounts[guess] = count
+        return count
+    }
+
+    return -1
+}
+
+
+
+
+
+
+function toggleSettingsPopup() {
+    //FOR SOME FUCKING REASON, THE SETTINGS POPUP TOGGLE ONLY WORKS THE SECOND TIME YOU CLICK IT, THE FIRST TIME THE DISPLAY CLAIMS TO BE UNDEFINED?!??!?!?
+    //NEVERMIND I GOT IT GET FUCKED ^^^^ 
+    let settingsPopup = document.getElementById("settingsPopup")
+    let popupBackground = document.getElementById("popupBackground")
+    console.log(settingsPopup.style.display)
+    if(settingsPopup.style.display == "none") {
+        settingsPopup.style.display = "block"
+        popupBackground.style.display = "block"
+    } else {
+        settingsPopup.style.display = "none"
+        popupBackground.style.display = "none"
+    }
+}
+
+function toggleVictoryPopup() {
+    let victoryPopup = document.getElementById("victoryPopup")
+    let popupBackground = document.getElementById("popupBackground")
+    console.log(victoryPopup.style.display)
+    if(victoryPopup.style.display == "none") {
+        victoryPopup.style.display = "block"
+        popupBackground.style.display = "block"
+    } else {
+        victoryPopup.style.display = "none"
+        popupBackground.style.display = "none"
+    }
+}
+
+function giveUp() {
+    toggleSettingsPopup()
+    displayText = articleText
+    document.getElementById("article").innerHTML = displayText
+}
+
+function newGame() {
+    displayText = ""
+    articleText = ""
+    data
+    articleName = vitalArticles[Math.floor(Math.random()*vitalArticles.length)]
+    //articleName = "pacific_ocean"
+    guesses = []
+    guessCounts = {}
+    articleWords
+
+    settingsPopup.style.display = "none"
+    victoryPopup.style.display = "none"
+    popupBackground.style.display = "none"
+
+    won = false
+
+    notCensored = [" ", ",", ".", "-", "(", ")", ";", ":", "'", "[", "]", "/"]
+
+    const http = new XMLHttpRequest()
+
+    http.open("GET", `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=${articleName}&redirects=true&origin=*`)
+    http.send()
+
+    http.onload = () => {
+        data = http.responseText
+        data = JSON.parse(data)
+        data = data.query.pages[Object.keys(data.query.pages)[0]]
+        load()
+    }
+}
+
+function share() {
+
+}
+
+
+function toggleTheme() {
+
+
+
+}
